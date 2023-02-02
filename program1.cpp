6e291e7c58c5bbffeb89adf5f319fc22391cb4a9
seanva1116 <112670823+seanva1116@users.noreply.github.com>
2023-02-02 14:55:55 -0500

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -2,39 +2,55 @@
 #include <fstream>
 #include <getopt.h>
 
+void usage();
+
 int main(int argc, char* argv[]){
 	std::ofstream out("P1_VargasMok.txt", std::ios::out | std::ios::app);
 	int ch, optx;
 
-	
+	std::cout <<"-----------------------------------" <<std::endl;
+	out <<"-----------------------------------" <<std::endl;
+
+	std::cout <<"Command " <<argv[0] <<std::endl;
+	out <<"Command " <<argv[0] <<std::endl;
+
 	for (;;){
-		ch = getopt(argc, argv, "qwerty");
+		ch = getopt(argc, argv, "ioaul");
 
 		if (ch == -1) break;
 		switch (ch){
+			case 'i':
+				std::cout <<"Switch " <<char(ch) <<std::endl;
+				out <<"Switch " <<char(ch) <<std::endl;
+				break;
+			case 'o':
+				std::cout <<"Switch " <<char(ch) <<std::endl;
+				out <<"Switch " <<char(ch) <<std::endl;
+				break;
 			case 'q':
 				std::cout <<"Switch " <<char(ch) <<std::endl;
+				out <<"Switch " <<char(ch) <<std::endl;
 				break;
-			case 'w':
+			case 'u':
 				std::cout <<"Switch " <<char(ch) <<std::endl;
+				out <<"Switch " <<char(ch) <<std::endl;
 				break;
-			case 'e':
+			case 'l':
 				std::cout <<"Switch " <<char(ch) <<std::endl;
+				out <<"Switch " <<char(ch) <<std::endl;
 				break;
-			case 'r':
-				std::cout <<"Switch " <<char(ch) <<std::endl;
+			default:
+				usage();
 				break;
-			case 't':
-				std::cout <<"Switch " <<char(ch) <<std::endl;
-				break;
-			case 'y':
-				std::cout <<"Switch " <<char(ch) <<std::endl;
-				break; 
 		}
 	}
 
 	for (optx = optind; optx < argc; optx++){
 		std::cout <<"Arguement " <<argv[optx] <<std::endl;
+		out <<"Arguement " <<argv[optx] <<std::endl;
 	}
 }
 
+void usage(){
+	std::cout <<"Usage: program1 [-ioaul] [arguments]\n"; 
+}
