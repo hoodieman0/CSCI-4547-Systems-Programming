e7a90cd2c644563a9903da99a5da65afcf555fe3
James Mok <hoodieman8@gmail.com>
2023-02-18 02:46:30 -0500

    Added FileID.hpp

---
+++ FileID.hpp
@@ -0,0 +1,31 @@
+/*
+CSCI-4547 Systems Programming
+FileID.hpp
+Author: James Mok
+Created: 18 Feb 2023
+Last edited: 18 Feb 2023
+*/
+
+#ifndef ID_HPP
+#define ID_HPP
+
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class FileID{
+    private:
+        char* filename;
+        int iNode;
+        string pathname;
+        vector<string> keywords;
+    
+    public:
+        // What is the type being talked about?
+        FileID(char* filename, int type, string path) : filename(filename), pathname(path) {}
+        void print(ostream& out);
+        void addKeyword(string word);
+};
+
+#endif
