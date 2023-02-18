d3e2e48f5483dba4bf65de27fef976bb2faa3c69
James Mok <hoodieman8@gmail.com>
2023-02-18 02:59:33 -0500

    defined FileID::addKeyword()

--- FileID.cpp
+++ FileID.cpp
@@ -10,12 +10,19 @@ Last edited: 18 Feb 2023
 
 // Prints the iNode ID and the path to the file
 // Precondition: valid FileID object
-// Postcondition: sends class data to ostream& out
+// Postcondition: sends class data to "ostream& out"
 void FileID::
 print(ostream& out){
     out << "iNode Number: " << iNode << "\t | Path: " << pathname << endl;
 }
 
-void FileID::addKeyword(string word){
-
+// Adds a new word for the class to find in the file
+// Precondition: valid FileID object
+// Postcondition: 'keywords" is updated with "word"
+void FileID::
+addKeyword(string word){
+    for(int idx = 0; idx < keywords.size(); idx++){
+        if (keywords[idx] == word){ return; }
+    }
+    keywords.push_back(word);
 }
