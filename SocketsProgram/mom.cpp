81723f2ecfa6c4d72b1046616e1874144637efcd
hoodieman0 <hoodieman8@gmail.com>
2023-04-30 20:36:17 -0400

    created mom class outline

---
+++ SocketsProgram/mom.cpp
@@ -0,0 +1,5 @@
+#include "mom.hpp"
+
+Mom::Mom(){
+    banner();
+}
---
+++ SocketsProgram/mom.hpp
@@ -0,0 +1,13 @@
+#ifndef MOM_HPP
+#define MOM_HPP
+
+#include "tools.hpp"
+#include "enums.hpp"
+
+class Mom {
+private:
+public:
+Mom();
+};
+
+#endif
