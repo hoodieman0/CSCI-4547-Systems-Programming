b5fdf57209bd67836340c235316d6d2f7f7aa670
hoodieman0 <hoodieman8@gmail.com>
2023-05-04 16:42:07 -0400

    Added jobTable class

---
+++ SocketsProgram/jobTable.hpp
@@ -0,0 +1,17 @@
+#ifndef JOBTABLE_HPP
+#define JOBTABLE_HPP
+
+#include "job.hpp"
+
+class JobTable {
+    public: 
+    Job jobs[10];
+
+    JobTable() { 
+        for(int i = 0; i < 10; i ++){
+		    jobs[i] = Job();
+	    }
+    }
+};
+
+#endif
