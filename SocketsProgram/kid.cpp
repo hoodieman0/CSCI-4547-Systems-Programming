bdf9cc34c54edc98ea9b4f416c373c9a56684dd8
hoodieman0 <hoodieman8@gmail.com>
2023-05-05 01:52:28 -0400

    Created kid file

--- SocketsProgram/kid.cpp
+++ SocketsProgram/kid.cpp
@@ -1,1 +1,18 @@
-#include "kid.hpp"
+#include "tools.hpp"
+#include "Socket.hpp"
+
+#define PORT 1099
+
+void printSockInfo( const char* who, sockInfo sock );
+
+// arg 1 is remote host
+int main(int argc, char* argv[]){
+    char* process = argv[0];
+    if ( argc != 2 ) fatal( "usage: %s hostname\n", process );
+
+    char* serverName = argv[1];
+
+    Socket client(process);
+    client.connect(serverName, PORT);
+
+}
