7d0dedac8cee09eec5f1d3c9bb4d8c2124e25e0a
James Mok <hoodieman8@gmail.com>
2023-05-11 03:53:14 -0400

    feat: added string array for sockStat variables for printing

--- SocketsProgram/enums.hpp
+++ SocketsProgram/enums.hpp
@@ -1,7 +1,11 @@
 #ifndef ENUMS_HPP
 #define ENUMS_HPP
 
+#include <string>
+
 enum sockStat {ACK, NACK, QUIT}; // used in mom.cpp
+static string sockString[] = {"ACK", "NACK", "QUIT"};
+
 enum jobStat {notStarted, working, complete}; // used in mom.cpp and kid.cpp
 enum mood {lazy, prissy, overtired, greedy, cooperative}; // used in kid
 
