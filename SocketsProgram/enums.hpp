eb23c16ac22dd420496eff3e22c26c14a898a8ec
James Mok <hoodieman8@gmail.com>
2023-05-11 03:26:16 -0400

    chore: updated enum comments

--- SocketsProgram/enums.hpp
+++ SocketsProgram/enums.hpp
@@ -1,8 +1,8 @@
 #ifndef ENUMS_HPP
 #define ENUMS_HPP
 
-enum sockStat {ACK, NACK, QUIT};
-enum jobStat {notStarted, working, complete};
-enum mood {lazy, prissy, overtired, greedy, cooperative};
+enum sockStat {ACK, NACK, QUIT}; // used in mom.cpp
+enum jobStat {notStarted, working, complete}; // used in mom.cpp and kid.cpp
+enum mood {lazy, prissy, overtired, greedy, cooperative}; // used in kid
 
 #endif
